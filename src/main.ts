8e1eef07e3f8b8c4a7cb541f4db649ba4e232b45
develar <develar@gmail.com>
2017-08-06 14:55:58 +0200

    feat: getConfig

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
-import { readFile } from "fs-extra-p"
+import { readFile, readJson } from "fs-extra-p"
 import { safeLoad } from "js-yaml"
 import * as path from "path"
+import { deepAssign } from "./deepAssign"
+import { Lazy } from "lazy-val"
 
 export async function readConfig<T>(configFile: string, projectDir?: string, log?: (message: string) => void): Promise<T> {
   const data = await readFile(configFile, "utf8")
@@ -45,4 +47,66 @@ export function orIfFileNotExist<T>(promise: Promise<T>, fallbackValue: T): Prom
       }
       throw e
     })
+}
+
+function getConfigFromPackageData(metadata: any | null, key: string) {
+  return metadata == null ? null : metadata[key]
+}
+
+export interface ReadConfigRequest {
+  key: string
+  projectDir: string
+  packageMetadata: Lazy<{ [key: string]: any } | null> | null
+
+  log?: (message: string) => void
+}
+
+export async function loadConfig<T>(request: ReadConfigRequest): Promise<T | null> {
+  let packageMetadata = request.packageMetadata == null ? null : await request.packageMetadata
+  if (packageMetadata == null) {
+    packageMetadata = await orNullIfFileNotExist(readJson(path.join(request.projectDir, "package.json")))
+  }
+  const data = getConfigFromPackageData(packageMetadata, request.key)
+  return data == null ? findAndReadConfig<T>(request.projectDir, request.key, request.log) : data
+}
+
+export async function getConfig<T>(request: ReadConfigRequest, configPath: string | null, configFromOptions: T | null | undefined): Promise<T> {
+  let fileOrPackageConfig: T | null
+  if (configPath == null) {
+    fileOrPackageConfig = await loadConfig<T>(request)
+  }
+  else {
+    fileOrPackageConfig = await readConfig<T>(path.resolve(request.projectDir, configPath), request.projectDir, request.log)
+  }
+
+  return deepAssign(fileOrPackageConfig == null ? Object.create(null) : fileOrPackageConfig, configFromOptions)
+}
+
+export async function loadParentConfig<T>(request: ReadConfigRequest, spec: string): Promise<T> {
+  let isFileSpec: boolean | undefined
+  if (spec.startsWith("file:")) {
+    spec = spec.substring("file:".length)
+    isFileSpec = true
+  }
+
+  let parentConfig = await orNullIfFileNotExist(readConfig<T>(path.resolve(request.projectDir, spec), request.projectDir, request.log))
+  if (parentConfig == null && isFileSpec !== true) {
+    let resolved: string | null = null
+    try {
+      resolved = require.resolve(spec)
+    }
+    catch (e) {
+      // ignore
+    }
+
+    if (resolved != null) {
+      parentConfig = await readConfig<T>(resolved, request.projectDir, request.log)
+    }
+  }
+
+  if (parentConfig == null) {
+    throw new Error(`Cannot find parent config file: ${spec}`)
+  }
+
+  return parentConfig
 }
